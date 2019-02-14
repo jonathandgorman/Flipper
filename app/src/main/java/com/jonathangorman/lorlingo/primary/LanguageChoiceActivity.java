92c8cbeb6c6ec66253b4e79a459ac239f5e9913c
Jonathan Gorman <jonathandgorman@gmail.com>
2019-02-14 23:46:08 +0100

    Addition of AdMob integration and coffee popup. AdMob has been integrated with a test Reward ad. A coffee popup is now available which when clicked triggers the reward ad.

--- app/src/main/java/com/jonathangorman/lorlingo/primary/LanguageChoiceActivity.java
+++ app/src/main/java/com/jonathangorman/lorlingo/primary/LanguageChoiceActivity.java
@@ -1,16 +1,29 @@
 package com.jonathangorman.lorlingo.primary;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 import com.jonathangorman.lorlingo.R;
 import com.jonathangorman.lorlingo.action.InfoDisplayActivity;
 import com.jonathangorman.lorlingo.tts.TTSManager;
@@ -18,13 +31,14 @@ import com.jonathangorman.lorlingo.adapter.LanguageChoiceAdapter;
 
 import java.util.ArrayList;
 
-public class LanguageChoiceActivity extends Activity {
+public class LanguageChoiceActivity extends Activity implements RewardedVideoAdListener {
 
     private static final String TAG = LanguageChoiceActivity.class.getName();
     private static final int TTS_ENGINE_CHECK_CODE = 0;
+    private static boolean TTS_AVAILABLE = false; // TTS engine available
 
     TTSManager ttsManager;
-    private static boolean TTS_AVAILABLE = false; // TTS engine available
+    private RewardedVideoAd mRewardedVideoAd;
 
     // TODO combine into single object
     ArrayList<Integer> languagesImagesList = new ArrayList<Integer>();
@@ -33,9 +47,16 @@ public class LanguageChoiceActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         Log.i(TAG, "ACTIVITY CREATE: Creating LanguageChoiceActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_choice);
+
+        // Initialise AdMob
+        MobileAds.initialize(this, "ca-app-pub-2251083820126124~2763503135");
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
+        mRewardedVideoAd.setRewardedVideoAdListener(this);
+        loadRewardedVideoAd();
 
         // initialise view lists
         initLanguageLists(); //TODO init lists based on language of the device
@@ -133,7 +154,6 @@ public class LanguageChoiceActivity extends Activity {
 
         Intent actionInfoIntent = new Intent(this, InfoDisplayActivity.class);
         switch (item.getItemId()) {
-
             case R.id.about_action:
                 actionInfoIntent.putExtra("INFOTYPE", "about"); // adapter position is removed from the list and added to intent
                 this.startActivity(actionInfoIntent);
@@ -147,12 +167,82 @@ public class LanguageChoiceActivity extends Activity {
                 this.startActivity(actionInfoIntent);
                 return true;
             case R.id.coffee_action:
-                actionInfoIntent.putExtra("INFOTYPE", "coffee"); // adapter position is removed from the list and added to intent
-                this.startActivity(actionInfoIntent);
+                createAlertDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    public void createAlertDialog()
+    {
+        // Create the alert dialog popup
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO AdMob integration if "ok" chosen - ca-app-pub-2251083820126124~2763503135
+                if (mRewardedVideoAd.isLoaded()) {
+                    mRewardedVideoAd.show();
+                }
+            }
+        }).setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {}
+        });
+
+        // Custom popup title set here
+        TextView title = new TextView(this);
+        title.setText("Your support is important!");
+        title.setBackgroundColor(getColor(R.color.colorPrimary));
+        title.setPadding(10, 15, 15, 10);
+        title.setGravity(Gravity.CENTER);
+        title.setTextColor(Color.WHITE);
+        title.setTextSize(22);
+
+        AlertDialog dialog = builder.create();
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogLayout = inflater.inflate(R.layout.coffee_popup, null);
+        dialog.setMessage(getString(R.string.coffee_text));
+        dialog.setCustomTitle(title);
+        dialog.setView(dialogLayout);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        dialog.show();
+    }
+
+    private void loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+                new AdRequest.Builder().build());
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        Toast.makeText(this, "Thanks for your support! :)", Toast.LENGTH_SHORT).show();
+        loadRewardedVideoAd(); // reload another add once finished
+    }
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "You left the app before watching the ad :(", Toast.LENGTH_SHORT).show();
+        loadRewardedVideoAd(); // reload another add once finished
+    }
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
+        Toast.makeText(this, "Oops... the ad isn't ready yet!", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onRewardedVideoAdLoaded() {
+    }
+    @Override
+    public void onRewardedVideoAdOpened() {
+    }
+    @Override
+    public void onRewardedVideoStarted() {
+    }
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+    }
+    @Override
+    public void onRewardedVideoCompleted() {
+    }
 }
